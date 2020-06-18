d8ea1aad4a66c16b998f63b2759c970dfe48bd1a
denismaua <denis.maua@gmail.com>
2020-06-18 10:40:03 -0300

    added our test file marginalization

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 # Driver script for all unit tests
 
-using Distributed
-@everywhere using Pkg
+# using Distributed
+# @everywhere using Pkg
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    # when run as a script, activate the test environment of this package so that test dependencies are available
-    @everywhere Pkg.activate(dirname(@__FILE__))
-    @everywhere Pkg.instantiate()
-end
+# if abspath(PROGRAM_FILE) == @__FILE__
+#     # when run as a script, activate the test environment of this package so that test dependencies are available
+#     @everywhere Pkg.activate(dirname(@__FILE__))
+#     @everywhere Pkg.instantiate()
+# end
 
-using Jive
+# using Jive
 
-runtests(@__DIR__, skip=["runtests.jl", "helper"])
+# runtests(@__DIR__, skip=["runtests.jl", "helper"])
+
+include("marginalization.jl")
